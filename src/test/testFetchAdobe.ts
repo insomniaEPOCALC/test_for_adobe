aa388495495e22a521a73f3101d1f7b77e4db73a
insomniaEPOCALC <pi-opal@outlook.jp>
2025-12-17 15:07:57 +0900

    fix: fix workflow

--- src/test/testFetchAdobe.ts
+++ src/test/testFetchAdobe.ts
@@ -1,13 +1,7 @@
+import { readFile } from "fs/promises";
+
 export async function fetchAdobeTerms() {
-  const url = 'https://www.adobe.com/jp/legal/terms.html'
-
-  const res = await fetch(url)
-  if (!res.ok) {
-    console.error('エラー:', res.status);
-    return
-  }
-
-  const html = await res.text();
+  const html = await readFile("adobe.html", "utf8");
 
   console.log(html);
 
