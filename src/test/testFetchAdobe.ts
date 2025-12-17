e68b8560c7acd2b42f3ac7c4ec61aabd3746baf1
insomniaEPOCALC <pi-opal@outlook.jp>
2025-12-17 12:03:17 +0900

    feat: add test program

---
+++ src/test/testFetchAdobe.ts
@@ -0,0 +1,16 @@
+export async function fetchAdobeTerms() {
+  const url = 'https://www.adobe.com/jp/legal/terms.html'
+
+  const res = await fetch(url)
+  if (!res.ok) {
+    console.error('エラー:', res.status)
+    return
+  }
+
+  const html = await res.text();
+
+  console.log(html)
+
+}
+
+fetchAdobeTerms().catch(console.error);
